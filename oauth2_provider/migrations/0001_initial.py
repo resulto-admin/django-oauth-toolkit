005ab3e84395c157a3137c875189d7c01d838161
Barthelemy Dagenais <bart@resulto.ca>
2019-06-14 09:49:37 -0400

    updated migrations to support django 2.x (bis)

--- oauth2_provider/migrations/0001_initial.py
+++ oauth2_provider/migrations/0001_initial.py
@@ -62,7 +62,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('token', models.CharField(max_length=255, db_index=True)),
-                ('access_token', models.OneToOneField(related_name='refresh_token', to='oauth2_provider.AccessToken')),
+                ('access_token', models.OneToOneField(related_name='refresh_token', to='oauth2_provider.AccessToken', on_delete=django.db.models.deletion.CASCADE)),
                 ('application', models.ForeignKey(to=oauth2_settings.APPLICATION_MODEL, on_delete=django.db.models.deletion.CASCADE)),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=django.db.models.deletion.CASCADE)),
             ],
